e8399b093e06dc325b4d5266262932fe80bf46a9
Niviru I.U. Weerasinghe <162433837+GHOSTXMAN2006@users.noreply.github.com>
2025-11-20 10:08:47 +0530

    Update Commit.bat

--- Commit.bat
+++ Commit.bat
@@ -1,14 +1,27 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Show modified or new files
+:: === SET YOUR PROJECT FOLDER HERE ===
+set "PROJECT=C:\Project"
+
+cd "%PROJECT%"
+
+:: Stage all modified and untracked files except skipped ones
+for /f "delims=" %%f in ('git ls-files --others --modified --exclude-standard') do (
+    if /i not "%%f"=="git-menu.bat" if /i not "%%f"=="Commit.bat" (
+        git add "%%f"
+    )
+)
+
+
+:: Show staged files
 echo ============================================
-echo Files changed and ready to commit:
+echo Files staged and ready to commit:
 echo ============================================
 git status -s
 
-:: Ask user for confirmation
-set /p confirm=Do you want to commit these changes??? (y/n): 
+:: Ask for confirmation
+set /p confirm=Do you want to commit these changes? (y/n): 
 if /i not "%confirm%"=="y" (
     echo Commit canceled.
     pause
@@ -18,7 +31,7 @@ if /i not "%confirm%"=="y" (
 :: Ask for commit message
 set /p msg=Enter commit message: 
 
-:: Commit staged changes
+:: Commit changes
 git commit -m "%msg%"
 
 if %errorlevel% neq 0 (
